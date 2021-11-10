f38c643b8ff448f6b1d1795aa0a2e2905e5b7bdc
gabrielacodep <anagabriela@codepsadecv.com>
2021-11-10 11:55:06 -0600

    Splash Screen lista

--- app/src/main/java/sv/edu/catolica/gabsshopapp/MainActivity.java
+++ app/src/main/java/sv/edu/catolica/gabsshopapp/MainActivity.java
@@ -2,13 +2,30 @@ package sv.edu.catolica.gabsshopapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Splash.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea,3000);
     }
 }
